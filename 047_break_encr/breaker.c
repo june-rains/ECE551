be871142fc80bfb5d469fd1454000c61f58c2590
cl580 <cl580@duke.edu>
2021-09-19 16:29:31 -0400

    first commit

--- 047_break_encr/breaker.c
+++ 047_break_encr/breaker.c
@@ -16,6 +16,13 @@ int count(FILE * f) {
   int max = 0;
   int currMax = arr[0];
   //注意要更新比较的数
+  /*或者可以写成:
+  for (int i = 0; i < 25; i++) {
+    if (arr[max] < arr[i + 1]) {
+      max = i + 1;
+    }
+  }
+  */
   for (int i = 1; i < 26; i++) {
     if (currMax < arr[i]) {
       max = i;
@@ -55,42 +62,3 @@ int main(int argc, char ** argv) {
   }
   return EXIT_SUCCESS;
 }
-
-/*
-int decrypt(FILE * f) {
-  int ch;
-  int cnt[26] = {0};
-  while ((ch = fgetc(f)) != EOF) {
-    if (isalpha(ch)) {
-      ch = tolower(ch);
-      cnt[ch - 'a'] += 1;
-    }
-  }
-  int mx = cnt[0];
-  int key = 0;
-  for (int i = 1; i < 26; i++) {
-    if (mx < cnt[i]) {
-      key = i;
-      mx = cnt[i];
-    }
-  }
-  //  printf("the most common char is %c\n", key + 'a');
-  return (key + 26 - 4) % 26;
-}
-int main(int argc, char ** argv) {
-  if (argc != 2) {
-    perror("The number of arguments is wrong");
-    return EXIT_FAILURE;
-  }
-  FILE * f = fopen(argv[1], "r");
-  if (f == NULL) {
-    perror("The file does not exist");
-    return EXIT_FAILURE;
-  }
-
-  printf("%d\n", decrypt(f));
-
-  fclose(f);
-  return EXIT_SUCCESS;
-}
-*/
