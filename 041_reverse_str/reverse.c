771f50f1cafb1698ed93fff2b485197ceb15f8e8
cl580 <cl580@duke.edu>
2021-09-18 20:50:43 -0400

    first commit

--- 041_reverse_str/reverse.c
+++ 041_reverse_str/reverse.c
@@ -3,7 +3,23 @@
 #include <string.h>
 
 void reverse(char * str) {
-  //WRITE ME!
+  char * ptr1 = str;
+  int count = 0;
+  while (*ptr1 != '\0') {
+    ptr1++;
+    count++;
+  }
+  char strcpy[count + 1];
+  strcpy[count] = '\0';
+  for (int i = 0; i < count; i++) {
+    ptr1--;
+    strcpy[i] = *ptr1;
+  }
+
+  for (int i = 0; i < count + 1; i++) {
+    *ptr1 = strcpy[i];
+    ptr1++;
+  }
 }
 
 int main(void) {
@@ -13,7 +29,8 @@ int main(void) {
   char str3[] = "Captain's log, Stardate 42523.7";
   char str4[] = "Hello, my name is Inigo Montoya.";
   char str5[] = "You can be my wingman anyday!";
-  char str6[] = "Executor Selendis! Unleash the full power of your forces! There may be no tomorrow!";
+  char str6[] = "Executor Selendis! Unleash the full power of your forces! There may be "
+                "no tomorrow!";
   char * array[] = {str0, str1, str2, str3, str4, str5, str6};
   for (int i = 0; i < 7; i++) {
     reverse(array[i]);
