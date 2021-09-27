d4fa9ba2b6f9976e24efea837f041bceed220040
cl580 <cl580@duke.edu>
2021-09-26 23:07:06 -0400

    first commit

--- 053_sort_lines/sortLines.c
+++ 053_sort_lines/sortLines.c
@@ -14,8 +14,50 @@ void sortData(char ** data, size_t count) {
   qsort(data, count, sizeof(char *), stringOrder);
 }
 
+int readAndSortAndPrint(FILE * f) {
+  size_t i = 0;
+  size_t sz = 0;
+  char * curr = NULL;
+  char ** lines = NULL;
+  while (getline(&curr, &sz, f) >= 0) {
+    lines = realloc(lines, (i + 1) * sizeof(*lines));
+    lines[i] = curr;
+    curr = NULL;
+    i++;
+  }
+  free(curr);
+  sortData(lines, i);
+  for (size_t j = 0; j < i; j++) {
+    printf("%s", lines[j]);
+    free(lines[j]);
+  }
+  free(lines);
+  return EXIT_SUCCESS;
+}
+
+void processOneFile(char * filename) {
+  FILE * f = fopen(filename, "r");
+  if (f == NULL) {
+    fprintf(stderr, "Can not open the file");
+    exit(EXIT_FAILURE);
+  }
+  readAndSortAndPrint(f);
+  if (fclose(f) != 0) {
+    fprintf(stderr, "Failed to close the input file!");
+    exit(EXIT_FAILURE);
+  }
+}
+
 int main(int argc, char ** argv) {
   //WRITE YOUR CODE HERE!
-
+  if (argc == 1) {
+    readAndSortAndPrint(stdin);
+  }
+  else {
+    for (int i = 1; i < argc; i++) {
+      processOneFile(argv[i]);
+    }
+    printf("finish all sorting and printing process!");
+  }
   return EXIT_SUCCESS;
 }
