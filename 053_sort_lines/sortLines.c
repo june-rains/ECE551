df3a1b94e8d6a3437b6cb0280ca1c248bfed4e25
cl580 <cl580@duke.edu>
2021-09-26 23:23:44 -0400

    first commit

--- 053_sort_lines/sortLines.c
+++ 053_sort_lines/sortLines.c
@@ -13,7 +13,7 @@ int stringOrder(const void * vp1, const void * vp2) {
 void sortData(char ** data, size_t count) {
   qsort(data, count, sizeof(char *), stringOrder);
 }
-
+/*
 int readAndSortAndPrint(FILE * f) {
   size_t i = 0;
   size_t sz = 0;
@@ -58,6 +58,49 @@ int main(int argc, char ** argv) {
       processOneFile(argv[i]);
     }
     printf("finish all sorting and printing process!");
+  }
+  return EXIT_SUCCESS;
+}
+*/
+
+void func(FILE * f) {
+  char * str = NULL;
+  size_t sz = 0;
+  int len = 0;
+  char ** str_array = NULL;
+  int cnt = 0;
+  while ((len = getline(&str, &sz, f)) != EOF) {
+    str_array = realloc(str_array, ++cnt * sizeof(*str_array));
+    str_array[cnt - 1] = str;
+    str = NULL;
+    sz = 0;
+  }
+  sortData(str_array, cnt);
+  for (int i = 0; i < cnt; i++) {
+    fprintf(stdout, "%s", str_array[i]);
+    free(str_array[i]);
+  }
+  free(str_array);
+  free(str);
+}
+int main(int argc, char ** argv) {
+  //WRITE YOUR CODE HERE!
+  if (argc == 1) {
+    func(stdin);
+  }
+  else {
+    for (int i = 1; i < argc; i++) {
+      FILE * f = fopen(argv[i], "r");
+      if (f == NULL) {
+        fprintf(stderr, "Can not find file");
+        exit(EXIT_FAILURE);
+      }
+      func(f);
+      if (fclose(f)) {
+        fprintf(stderr, "Failed to close file");
+        exit(EXIT_FAILURE);
+      }
+    }
   }
   return EXIT_SUCCESS;
 }
