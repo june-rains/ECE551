15c7e03613509ce558e3495df238e673866552b0
cl580 <cl580@duke.edu>
2021-10-05 10:22:27 -0400

    first commit

--- 057_outname/outname.c
+++ 057_outname/outname.c
@@ -1,8 +1,22 @@
+#include "outname.h"
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "outname.h"
 
 char * computeOutputFileName(const char * inputName) {
   //WRITE ME
+  size_t len = strlen(inputName);
+  size_t new_len = len + 8;
+  char * result = malloc(new_len * sizeof(*result));
+  result = strcpy(result, inputName);
+  result[len] = '.';
+  result[len + 1] = 'c';
+  result[len + 2] = 'o';
+  result[len + 3] = 'u';
+  result[len + 4] = 'n';
+  result[len + 5] = 't';
+  result[len + 6] = 's';
+  result[len + 7] = '\0';
+  return result;
 }
