7c0bf66000df06d55950c177ab76f7bb8accdadf
cl580 <cl580@duke.edu>
2021-10-05 15:34:58 -0400

    first commit

--- 058_counts/counts.c
+++ 058_counts/counts.c
@@ -1,17 +1,60 @@
+#include "counts.h"
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "counts.h"
+
 counts_t * createCounts(void) {
   //WRITE ME
+  counts_t * counts = malloc(sizeof(*counts));
+  counts->unknown_counts = 0;
+  counts->strs = NULL;
+  counts->len = 0;
+  return counts;
 }
+
+int find_string(counts_t * c, const char * name) {
+  for (int i = 0; i < c->len; i++) {
+    if (strcmp(name, c->strs[i].str) == 0) {
+      c->strs[i].counts++;
+      return 1;
+    }
+  }
+  return 0;
+}
+
 void addCount(counts_t * c, const char * name) {
   //WRITE ME
+  //unknown case
+  if (name == NULL) {
+    c->unknown_counts++;
+    return;
+  }
+  //normal case
+  if (!find_string(c, name)) {
+    c->strs = realloc(c->strs, (c->len + 1) * sizeof(*(c->strs)));
+    size_t sz = strlen(name) + 1;
+    c->strs[c->len].str = strndup(name, sz);
+    c->strs[c->len].counts = 1;
+    c->len++;
+  }
 }
+
 void printCounts(counts_t * c, FILE * outFile) {
   //WRITE ME
+  for (int i = 0; i < c->len; i++) {
+    fprintf(outFile, "%s: %d\n", c->strs[i].str, c->strs[i].counts);
+  }
+  if (c->unknown_counts != 0) {
+    fprintf(outFile, "<unknown>: %d\n", c->unknown_counts);
+  }
 }
 
 void freeCounts(counts_t * c) {
   //WRITE ME
+  for (int i = 0; i < c->len; i++) {
+    free(c->strs[i].str);
+  }
+  free(c->strs);
+  free(c);
 }
--- 058_counts/counts.h
+++ 058_counts/counts.h
@@ -1,13 +1,40 @@
 #ifndef __COUNTS_H__
 #define __COUNTS_H__
+#include <stdio.h>
+/*
 struct _one_count_t {
   //DEFINE ME
-
+  int count;
+  char * str;
 };
 typedef struct _one_count_t one_count_t;
 
 struct _counts_t {
   //DEFINE ME
+  one_count_t * arr;
+  int n;
+};
+typedef struct _counts_t counts_t;
+
+counts_t * createCounts(void);
+void addCount(counts_t * c, const char * name);
+void printCounts(counts_t * c, FILE * outFile);
+
+void freeCounts(counts_t * c);
+*/
+
+struct _one_count_t {
+  //DEFINE ME
+  char * str;
+  int counts;
+};
+typedef struct _one_count_t one_count_t;
+
+struct _counts_t {
+  //DEFINE ME
+  one_count_t * strs;
+  int len;
+  int unknown_counts;
 };
 typedef struct _counts_t counts_t;
 
