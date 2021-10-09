e2b0c9c16317c9695c865fcab0d36471463a3a3b
cl580 <cl580@duke.edu>
2021-10-08 21:47:06 -0400

    first commit

--- 064_vector_2d/vector.cpp
+++ 064_vector_2d/vector.cpp
@@ -5,3 +5,35 @@
 
 /* write your class implementation in this file
  */
+
+void Vector2D::initVector(double init_x, double init_y) {
+  x = init_x;
+  y = init_y;
+}
+double Vector2D::getMagnitude() const {
+  using std::sqrt;
+  return sqrt(dot(*this));
+}
+
+Vector2D Vector2D::operator+(const Vector2D & rhs) const {
+  Vector2D newVector;
+  newVector.x = x + rhs.x;
+  newVector.y = y + rhs.y;
+  return newVector;
+}
+
+Vector2D & Vector2D::operator+=(const Vector2D & rhs) {
+  x += rhs.x;
+  y += rhs.y;
+  return *this;
+}
+
+double Vector2D::dot(const Vector2D & rhs) const {
+  double result;
+  result = x * rhs.x + y * rhs.y;
+  return result;
+}
+
+void Vector2D::print() const {
+  std::printf("<%.2f, %.2f>", x, y);
+}
