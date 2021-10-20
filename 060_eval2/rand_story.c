0eeeb48996af1214c16532c423e06d8b9379e291
cl580 <cl580@duke.edu>
2021-10-19 20:32:05 -0400

    f

--- 060_eval2/rand_story.c
+++ 060_eval2/rand_story.c
@@ -109,40 +109,46 @@ wordarray_t * replaceAll(wordarray_t * arr,
     int restlen = strlen(rest);
     const char * new = NULL;
     /***************CATEGORY WITH NO REFERENCE NUMBER********/
-    if (atoi(catarr->wordArr[i]) == 0) {
-      new = strdup(chooseWord(catarr->wordArr[i], *ans));
-      (*usedWords)->words =
-          realloc((*usedWords)->words,
-                  ((*usedWords)->n_words + 1) * sizeof(*(*usedWords)->words));
-      (*usedWords)->words[(*usedWords)->n_words] = strdup(new);
-      (*usedWords)->n_words++;
+    char * endptr;
+    if (strtol(catarr->wordArr[i], &endptr, 10) == 0) {
+      if (endptr == catarr->wordArr[i]) {
+        new = strdup(chooseWord(catarr->wordArr[i], *ans));
+        (*usedWords)->words =
+            realloc((*usedWords)->words,
+                    ((*usedWords)->n_words + 1) * sizeof(*(*usedWords)->words));
+        (*usedWords)->words[(*usedWords)->n_words] = strdup(new);
+        (*usedWords)->n_words++;
 
-      /*************IF THERE HAS -n ARGUMENTS************/
-      if (rm == 1) {
-        for (size_t m = 0; m < (*ans)->n; m++) {
-          if (strcmp((*ans)->arr[m].name, catarr->wordArr[i]) == 0) {
-            if ((*ans)->arr[m].n_words == 0) {
-              fprintf(stderr, "words are used up! no more words!");
-              exit(EXIT_FAILURE);
-            }
-            for (size_t n = 0; n < (*ans)->arr[m].n_words; n++) {
-              if (strcmp((*ans)->arr[m].words[n], new) == 0) {
-                free((*ans)->arr[m].words[n]);
-                (*ans)->arr[m].words[n] = NULL;
-                for (size_t k = n; k < (*ans)->arr[m].n_words - 1; k++) {
-                  (*ans)->arr[m].words[k] = (*ans)->arr[m].words[k + 1];
+        /*************IF THERE HAS -n ARGUMENTS************/
+        if (rm == 1) {
+          for (size_t m = 0; m < (*ans)->n; m++) {
+            if (strcmp((*ans)->arr[m].name, catarr->wordArr[i]) == 0) {
+              if ((*ans)->arr[m].n_words == 0) {
+                fprintf(stderr, "words are used up! no more words!");
+                exit(EXIT_FAILURE);
+              }
+              for (size_t n = 0; n < (*ans)->arr[m].n_words; n++) {
+                if (strcmp((*ans)->arr[m].words[n], new) == 0) {
+                  free((*ans)->arr[m].words[n]);
+                  (*ans)->arr[m].words[n] = NULL;
+                  for (size_t k = n; k < (*ans)->arr[m].n_words - 1; k++) {
+                    (*ans)->arr[m].words[k] = (*ans)->arr[m].words[k + 1];
+                  }
+                  (*ans)->arr[m].words = realloc(
+                      (*ans)->arr[m].words,
+                      ((*ans)->arr[m].n_words - 1) * sizeof(*(*ans)->arr[m].words));
+                  (*ans)->arr[m].n_words--;
                 }
-                (*ans)->arr[m].words =
-                    realloc((*ans)->arr[m].words,
-                            ((*ans)->arr[m].n_words - 1) * sizeof(*(*ans)->arr[m].words));
-                (*ans)->arr[m].n_words--;
               }
             }
           }
         }
       }
+      else {
+        fprintf(stderr, "Invalid format of the category number!");
+        exit(EXIT_FAILURE);
+      }
     }
-
     /***************CATEGORY WITH VALID REFERENCE NUMBER***********/
     else if (atoi(catarr->wordArr[i]) >= 1 &&
              atoi(catarr->wordArr[i]) <= (int)(*usedWords)->n_words) {
